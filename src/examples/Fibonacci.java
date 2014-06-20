63214771edd33a37bd694165f5f53ea552d7d9ed
harukogaki <sariza2@uic.edu>
2014-06-20 12:47:13 -0500

    for some reason

--- src/examples/Fibonacci.java
+++ src/examples/Fibonacci.java
@@ -4,6 +4,7 @@ import gov.nasa.jpf.vm.Verify;
 public class Fibonacci { //test comment for vincent pull (you guys suck) <--Steph added to this
 						// I made a dead baby and a wet dog
 						//RawrRawrRawr
+	
 	public static int ok_fib(int num)
 	{
 		if (num == 0)

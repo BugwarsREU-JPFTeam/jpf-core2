1199d212c0589a6cec025fdc25471d765487439d
harukogaki <sariza2@uic.edu>
2014-06-20 12:44:38 -0500

    I created a program that implements the Verify tags in jpf-core. This program is essentially a needle in a haystack where the only fault is hidden in a (branch factor)^(depth) amount of possible executions.

---
+++ src/examples/recursive_branching_jpf.java
@@ -0,0 +1,35 @@
+import gov.nasa.jpf.vm.Verify;
+
+
+public class recursive_branching_jpf {
+	
+	public static void branch_recursive (int max_val, int depth, int branch, int sum)
+	{
+		if (depth == 0)
+		{
+			System.out.println("Obtained sum of " + sum);
+			return;
+		}
+		
+		int rand_val= Verify.random(branch);
+		System.out.println("Value obtained is " + rand_val);
+		sum += rand_val;
+		
+		if (sum == max_val){
+			System.out.println("This is wrong!!");
+			
+		}
+		
+		branch_recursive(max_val,depth-1,branch,sum);
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int depth = 3;
+		int branch = 5;
+		branch_recursive(depth*branch,depth, branch, 0);
+		
+	}
+
+}

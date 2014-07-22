4f9291aa159e2f078ec2c54a09a915c2aa4c51a0
Maxwell Daum <daumisgod@gmail.com>
2014-07-22 13:59:38 -0500

    added a new test case....possibly used for explanation....

---
+++ src/examples/BranchCountExplanationExample.java
@@ -0,0 +1,32 @@
+import gov.nasa.jpf.vm.Verify;
+
+public class BranchCountExplanationExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = Verify.random(1);
+		System.out.println("chose a a! Value is " + a);
+		int b = Verify.random(1);
+		System.out.println("chose a b! Value is " + b);
+		int c = Verify.random(1);
+		System.out.println("chose a c! Value is " + c);
+		for (int i = 0; i < a; i++) {
+			System.out.println(i + "th run of first loop");
+			System.out.println("a,b,c,i:" + a + "," + b + "," + c + "," + i);
+		}
+		for (int j = 0; j < b; j++) {
+			System.out.println(j + "th run of second loop");
+			System.out.println("a,b,c,j:" + a + "," + b + "," + c + "," + j);
+		}
+		for (int k = 0; k < c; k++) {
+			System.out.println(k + "th run of third loop");
+			System.out.println("a,b,c,k:" + a + "," + b + "," + c + "," + k);
+		}
+
+		System.out.println("------------");
+	}
+
+}

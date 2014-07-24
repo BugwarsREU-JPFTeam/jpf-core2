d8cc70b08ab9184259a3c7aeef9c12797cacbf7a
harukogaki <sariza2@uic.edu>
2014-07-24 14:31:13 -0500

    more changes :/

--- src/examples/branch_prog_2.java
+++ src/examples/branch_prog_2.java
@@ -4,13 +4,15 @@ import gov.nasa.jpf.vm.Verify;
 public class branch_prog_2 {
 	
 	public static void branching(){
-		int num1 = Verify.random(2);
-		int num2 = Verify.random(2);
-		int num3 = Verify.random(2);
+		int num1 = Verify.random(4);
+		int num2 = Verify.random(4);
+		int num3 = Verify.random(4);
+		int num4 = Verify.random(4);
+		int num5 = Verify.random(4);
 		
 		int sum = num1 + num2 + num3;
 		
-		if (num1 == 0 && num2 ==1 && num3 ==0){
+		if (num1 == 1 && num2 ==0 && num3 ==0 && num4 ==0 && num5 ==0){
 			throw new IllegalArgumentException("Found it");
 		}
 	}

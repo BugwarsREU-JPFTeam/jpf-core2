14171c92b06c0f2c0f38e03dea33f13f15414361
Maxwell Daum <daumisgod@gmail.com>
2014-07-28 17:54:19 -0500

    confirmed....branch counting works on other examples....

--- src/examples/branch_prog_2.java
+++ src/examples/branch_prog_2.java
@@ -4,10 +4,10 @@ import gov.nasa.jpf.vm.Verify;
 public class branch_prog_2 {
 	
 	public static void branching(){ //be sure to adjust depth with number of vars
-		int num1 = Verify.random(3); //and branch with the verify tag - 1
-		int num2 = Verify.random(3);
-		int num3 = Verify.random(3);
-		int num4 = Verify.random(3);
+		int num1 = Verify.getInt(0,3); //and branch with the verify tag - 1
+		int num2 = Verify.getInt(0,3); 
+		int num3 = Verify.getInt(0,3); 
+		int num4 = Verify.getInt(0,3); 
 		
 		
 		int sum = num1 + num2 + num3;
